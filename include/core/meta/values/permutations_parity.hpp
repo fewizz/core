8d763db7b2cc58e330fa44ea296925ece3e06599
fewizz <hazeevaidar@gmail.com>
2022-02-25 23:15:47 +0300

    ...

---
+++ include/core/meta/values/permutations_parity.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "../../integer.hpp"
+
+namespace values {
+
+	// TODO rename, redo
+	struct permutations_parity {
+
+		template<nuint Result, nuint... Indices>
+		struct index {
+			static constexpr nuint result = Result;
+		};
+
+		template<nuint Result, nuint Index, nuint... OtherIndices>
+		requires(sizeof...(OtherIndices) > 0)
+		struct index<Result, Index, OtherIndices...> {
+
+			static constexpr nuint result =
+				index<
+					Result + ((Index > OtherIndices) + ...),
+					OtherIndices...
+				>::result;
+
+		};
+
+		template<nuint... Indices>
+		static constexpr nuint for_indices = index<0, Indices...>::result;
+	};
+
+}
