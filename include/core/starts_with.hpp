efbbc48ca4b8b25701dd1c86396d52333f447e27
fewizz <hazeevaidar@gmail.com>
2022-06-22 21:15:52 -0700

    starts_with

---
+++ include/core/starts_with.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "range.hpp"
+#include "to_range.hpp"
+#include "equals.hpp"
+
+template<range Range>
+struct starts {
+	Range&& range_;
+
+	constexpr starts(Range&& range) : range_{ range } {}
+
+	template<range OtherRange>
+	constexpr bool with(OtherRange&& other) const {
+		auto size = range_.size();
+		auto other_size = other.size();
+
+		if(size < other_size) return false;
+
+		return equals(
+			to_range(
+				range_.begin(), range_.begin() + other_size
+			),
+			other
+		);
+	}
+
+};
+
+template<range Range>
+starts(Range&&) -> starts<Range>;
