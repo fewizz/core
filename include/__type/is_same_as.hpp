49d45e209ee0429eb3dd7ba8bd0dc32549f04bc7
fewizz <hazeevaidar@gmail.com>
2022-12-14 16:45:43 +0300

    ...

--- include/__type/is_same_as.hpp
+++ include/__type/is_same_as.hpp
@@ -2,13 +2,16 @@
 
 namespace __type {
 
-	template<typename Type, typename BaseType>
-	constexpr inline bool is_same_as = __is_same_as(Type, BaseType);
+	template<typename Type, typename OtherType>
+	constexpr inline bool is_same_as = __is_same_as(Type, OtherType);
 
 }
 
-template<typename Type, typename... BaseTypes>
-concept same_as = (__is_same_as(Type, BaseTypes) || ...);
+template<typename Type, typename OtherType>
+concept same_as = __is_same_as(Type, OtherType);
 
-template<typename Type, typename BaseType>
-concept not_same_as = !__is_same_as(Type, BaseType);; // genious
+template<typename Type, typename... OtherTypes>
+concept same_as_any = (same_as<Type, OtherTypes> || ...);
+
+template<typename Type, typename OtherType>
+concept not_same_as = !same_as<Type, OtherType>; // genious
